58fc06c82c42e5225bde729fea5b8ecedc57d9ee
Erick Zabala <erickzabala@hotmail.com>
2023-03-02 21:58:14 -0600

    Update README.md using PyATS
    ./network_inventory.py nso_sandbox_testbed_same_credentials.yaml

--- network_inventory.py
+++ network_inventory.py
@@ -1,4 +1,4 @@
-#!/home/opc/DEVNET/AUTOMATING-A-NETWORK-INVENTORY-WITH-PYTHON/inventory/bin/python
+#!/home/devnet/Documents/AUTOMATING-A-NETWORK-INVENTORY-WITH-PYTHON/inventory/bin/python
 """
 That is an script basic that permit capture an inventory of our network 
 using CLI as "show version" and "show inventory".
